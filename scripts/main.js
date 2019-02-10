342f4a0c812dcb03d1531c73da9e7832a444534f
graycatbird <47507891+graycatbird@users.noreply.github.com>
2019-02-10 15:43:13 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+var mySrc = myImage.getAttribute('src');
+if (mySrc === 'images/asclepias-tuberosa.jpg') {
+myImage.setAttribute ('src', 'images/prairie.jpg');
+} else {
+	myImage.setAttribute ('src', 'images/asclepias-tuberosa.jpg');
+	}
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+var myName = prompt('Please enter your name.');
+localStorage.setItem('name', myName);
+myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+	setUserName();
+}
